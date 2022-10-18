de980b31e6c2db90e59c5ccb33dc4e2cc24c3d91
lucas.erler <lucas.erler@hotmail.com>
2022-10-18 17:38:55 -0300

    logica do calculo

--- calc.js
+++ calc.js
@@ -1,1 +1,49 @@
-'use strict'
+"use strict";
+
+const display = document.getElementById("display");
+const numeros = document.querySelectorAll("[id*=tecla]");
+const operadores = document.querySelectorAll("[id*=operador]");
+
+let operador;
+let primeiroNumero;
+let novoNumero = true;
+
+const inserirNumero = (evento) => atualizarDsiplay(evento.target.textContent);
+const operacaoParada = () => operador !== undefined;
+
+const igual = () => { 
+    calcular(); 
+    operador = undefined;
+};
+document.getElementById("resultado").addEventListener('click', igual);
+
+const calcular = () => {
+  if (operacaoParada()) {
+    const ultimoNumero = parseFloat(display.textContent);
+    novoNumero = true;
+    const resultado =eval(`${primeiroNumero}${operador}${ultimoNumero}`);
+    atualizarDsiplay(resultado);
+  }
+};
+
+const atualizarDsiplay = (texto) => {
+  if (novoNumero) {
+    display.textContent = texto;
+    novoNumero = false;
+  } else {
+    display.textContent += texto;
+  }
+};
+
+const selecionarOperador = (evento) => {
+  if (!novoNumero) {
+    calcular();
+    novoNumero = true;
+    operador = evento.target.textContent;
+    primeiroNumero = parseFloat(display.textContent.replace('.' , ' ').replace(',' , '.'));
+    //console.log(operador);
+  }
+};
+
+numeros.forEach((numero) => numero.addEventListener("click", inserirNumero));
+operadores.forEach((operador) => operador.addEventListener("click", selecionarOperador));
