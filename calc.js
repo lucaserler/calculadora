561c829a81fbe64a50cbaa739c62859fa07ad2c2
lucas.erler <lucas.erler@hotmail.com>
2022-10-19 12:12:16 -0300

    Ajuste

--- calc.js
+++ calc.js
@@ -12,57 +12,60 @@ const inserirNumero = (evento) => atualizarDsiplay(evento.target.textContent);
 const operacaoParada = () => operador !== undefined;
 
 const apagarCalculo = () => {
-    limparDisplay();
-    operador = undefined;
-    novoNumero = true;
-    primeiroNumero = undefined;
-}
-document.getElementById('apagarCalculo').addEventListener('click', apagarCalculo);
-
-const limparDisplay = () => display.textContent = ' ';
-document.getElementById("limparDisplay").addEventListener('click', limparDisplay);
-
-const igual = () => { 
-    calcular(); 
-    operador = undefined;
+  limparDisplay();
+  operador = undefined;
+  novoNumero = true;
+  primeiroNumero = undefined;
 };
-document.getElementById("resultado").addEventListener('click', igual);
+document
+  .getElementById("apagarCalculo")
+  .addEventListener("click", apagarCalculo);
+
+const limparDisplay = () => (display.textContent = " ");
+document
+  .getElementById("limparDisplay")
+  .addEventListener("click", limparDisplay);
+
+const igual = () => {
+  calcular();
+  operador = undefined;
+};
+document.getElementById("resultado").addEventListener("click", igual);
 
 const trocarSinal = () => {
   novoNumero = true;
   atualizarDsiplay(display.textContent * -1);
-}
-document.getElementById("PN").addEventListener('click', trocarSinal);
+};
+document.getElementById("PN").addEventListener("click", trocarSinal);
 
-
-const existeDecimal = () => display.textContent.indexOf(',') != -1;
+const existeDecimal = () => display.textContent.indexOf(',') !== -1;
 const existeNumero = () => display.textContent.length > 0;
 const decimal = () => {
-  if(!existeDecimal()){
-    if(existeNumero()){
-      atualizarDsiplay(',')
-    }else{
-      atualizarDsiplay('0,')
+  if (!existeDecimal()) {
+    if (novoNumero) {
+      atualizarDsiplay('0,');
+    } else {
+      atualizarDsiplay(',');
     }
   }
-}
-document.getElementById("virgula").addEventListener('click', decimal)
+};
+document.getElementById("virgula").addEventListener("click", decimal);
 
 const calcular = () => {
   if (operacaoParada()) {
-    const ultimoNumero = parseFloat(display.textContent.replace(',' , '.'));
+    const ultimoNumero = parseFloat(display.textContent.replace(".", " ").replace(',', '.'));
     novoNumero = true;
-    const resultado =eval(`${primeiroNumero}${operador}${ultimoNumero}`);
+    const resultado = eval(`${primeiroNumero}${operador}${ultimoNumero}`);
     atualizarDsiplay(resultado);
   }
 };
 
 const atualizarDsiplay = (texto) => {
   if (novoNumero) {
-    display.textContent = texto.toLocaleString('BR');
+    display.textContent = texto.toLocaleString("BR");
     novoNumero = false;
   } else {
-    display.textContent += texto.toLocaleString('BR');
+    display.textContent += texto.toLocaleString("BR");
   }
 };
 
@@ -71,13 +74,12 @@ const selecionarOperador = (evento) => {
     calcular();
     novoNumero = true;
     operador = evento.target.textContent;
-    primeiroNumero = parseFloat(display.textContent.replace('.' , '.'));
+    primeiroNumero = parseFloat(display.textContent.replace(".", "."));
     //console.log(operador);
   }
 };
 
 numeros.forEach((numero) => numero.addEventListener("click", inserirNumero));
-operadores.forEach((operador) => operador.addEventListener("click", selecionarOperador));
-
-/////////////////////////////////RECONHECIMENTO-TECLADO/////////////////////////////////
-
+operadores.forEach((operador) =>
+  operador.addEventListener("click", selecionarOperador)
+);
